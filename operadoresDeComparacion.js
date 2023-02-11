3905f335e88e5072fe02f1284bdde22d69597f47
loppezzitto <lopez.emmanuelmatias@gmail.com>
2023-02-10 21:52:37 -0300

    Actualizacion de archivos

---
+++ operadoresDeComparacion.js
@@ -0,0 +1,6 @@
+//
+
+consola.log (4 < 7);
+consola.log (4 < 1);
+consola.log (4 > 4);
+consola.log (4 == 7);
---
+++ variables.js
@@ -0,0 +1,22 @@
+//String ->(cadenas de textos)
+var string ="textos";
+
+//Numeros
+var numerosEnteros = (1,2,3)
+var numerosNegativos = (-1, -2, -3)
+var numerosDecimales = (0.1, 2.5, 3.1 )
+
+
+//Booleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1;
+console.log (cajon1);
+
+//Null
+
+var sinNada = null;
